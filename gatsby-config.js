491a24215d75f77ea25172e941c9d9089afb3265
keithalpichi <keithalpichi@gmail.com>
2019-04-15 18:28:27 -0700

    Remove prismjs code highlighting and use simple styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,16 +52,16 @@ module.exports = {
     },
     {
       resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              classPrefix: "language-",
-            },
-          },
-        ],
-      },
+      // options: {
+      //   plugins: [
+      //     {
+      //       resolve: `gatsby-remark-prismjs`,
+      //       options: {
+      //         classPrefix: "language-",
+      //       },
+      //     },
+      //   ],
+      // },
     },
   ]
 }
