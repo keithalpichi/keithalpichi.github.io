55c4a5520b98d9f352a6cc42888d54e8d8b1be5d
keithalpichi <keithalpichi@gmail.com>
2019-04-28 22:35:03 -0700

    Edit 404 page copy and style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
+import { css } from 'linaria'
+
+const className = css`
+  text-align: center;
+`
 
 const About: React.SFC = () => (
   <React.Fragment>
     <Helmet title='Keith Alpichi | Page Not Found' />
-    <h1>Oh brah! No can find da' page you're looking for</h1>
+    <h1 className={className}>404</h1>
+    <h2 className={className}>Oh brah! No can find da' page you're looking for</h2>
   </React.Fragment>
 )
 
