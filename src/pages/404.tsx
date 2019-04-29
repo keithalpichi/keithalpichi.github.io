22cf75d62e49c310310209a95ed2f435ad94c010
keithalpichi <keithalpichi@gmail.com>
2019-04-28 22:28:59 -0700

    Edit 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet'
 const About: React.SFC = () => (
   <React.Fragment>
     <Helmet title='Keith Alpichi | Page Not Found' />
-    <h1>Oh brah! I cannot find da' kine page you're looking for</h1>
+    <h1>Oh brah! No can find da' page you're looking for</h1>
   </React.Fragment>
 )
 
