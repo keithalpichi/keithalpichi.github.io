f891c600781711a7efcd7aadb8be6b9aabac90d3
Keith Alpichi <keithalpichi@gmail.com>
2020-10-24 23:27:00 -0700

    edit colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,17 +10,15 @@ module.exports = {
   },
   theme: {
     extend: {
-      extend: {
-        colors: {
-          cream: '#FDF7EC',
-          nude: '#FAEFDC',
-          navy: '#515374',
-          teal: '#88C4C8',
-          mustard: '#F7C257',
-          dijon: '#E5BD64',
-          burnt: '#DC885D',
-          wasabi: '#9CA97D'
-        }
+      colors: {
+        cream: '#FDF7EC',
+        nude: '#FAEFDC',
+        navy: '#2F4A72',
+        teal: '#88C4C8',
+        mustard: '#F7C257',
+        dijon: '#E5BD64',
+        burnt: '#DC885D',
+        wasabi: '#9CA97D'
       },
       fontFamily: {
         bebas: ['Bebas Neue', 'Roboto', ...defaultTheme.fontFamily.sans],
