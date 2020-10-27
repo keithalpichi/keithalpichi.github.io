7ae690b2d1dc7c3558724c1d70f9cd447164505e
Keith Alpichi <keithalpichi@gmail.com>
2020-10-26 23:16:31 -0700

    Edit 404 to use Main

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
+import Main from '../layouts'
 
 export default function () {
   return (
-    <React.Fragment>
+    <Main>
       <Helmet title='Page Not Found' />
       <div className='text-center'>
         <h1>404</h1>
         <h2>Oh brah! No can find da' page you're looking for.</h2>
       </div>
-    </React.Fragment>
+    </Main>
   )
 }
