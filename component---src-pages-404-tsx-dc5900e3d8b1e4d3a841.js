a71a449759d82b4591421fc17e59be787f98638b
Keith Alpichi <keithalpichi@gmail.com>
2020-10-26 23:17:24 -0700

    Updates

---
+++ component---src-pages-404-tsx-dc5900e3d8b1e4d3a841.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n("qhky"),o=n("+ego");t.default=function(){return a.createElement(o.a,null,a.createElement(l.a,{title:"Page Not Found"}),a.createElement("div",{className:"text-center"},a.createElement("h1",null,"404"),a.createElement("h2",null,"Oh brah! No can find da' page you're looking for.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-dc5900e3d8b1e4d3a841.js.map
